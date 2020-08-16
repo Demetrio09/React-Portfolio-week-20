a32541ff55d5519bd6e438ae0b9bb1d117c39b71
Demetrio <demetrioalmeida@gmail.com>
2020-08-15 23:40:47 -0700

    functional homepage

--- react_portfolio/src/App.js
+++ react_portfolio/src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
 import Header from "./components/Header";
-import Util from "./utils/Header";
 import Project from "./components/Project";
-
+import Util from "./utils/Header/index.json";
 
 function App() {
-  // console.log(x)
   return (
     <div>
       <Header {...Util}></Header>
