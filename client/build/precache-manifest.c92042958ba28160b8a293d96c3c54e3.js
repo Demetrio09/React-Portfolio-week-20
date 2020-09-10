32c9892a66f1df9134e1a64fc2e724ac5ea41ba3
Demetrio <demetrioalmeida@gmail.com>
2020-09-10 13:11:47 -0400

    deploying to heroku

---
+++ client/build/precache-manifest.c92042958ba28160b8a293d96c3c54e3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e843c5812b08bbba7a5ccf18ed2f1c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48d23ffa2d4db70ffaf7",
+    "url": "/static/css/main.bbf89524.chunk.css"
+  },
+  {
+    "revision": "d2879bffb4e681ffa1c9",
+    "url": "/static/js/2.08b584ff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.08b584ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48d23ffa2d4db70ffaf7",
+    "url": "/static/js/main.325f9c83.chunk.js"
+  },
+  {
+    "revision": "a36784af4abdca6cc163",
+    "url": "/static/js/runtime-main.ea1ad2b7.js"
+  },
+  {
+    "revision": "d79d24a187195b31f1085ff5ac6283aa",
+    "url": "/static/media/Eat-DA-Burger.d79d24a1.jpg"
+  },
+  {
+    "revision": "f6281816fc0beb50768b76a5204aa3fe",
+    "url": "/static/media/Screen Shot 2020-05-09 at 10.26.09 AM.f6281816.jpg"
+  },
+  {
+    "revision": "ac9acf3e6416addf05b5a31f3c27a3c2",
+    "url": "/static/media/Screen Shot 2020-07-11 at 10.50.23 AM.ac9acf3e.png"
+  },
+  {
+    "revision": "8f2979b7a57af33538c244b6496349a0",
+    "url": "/static/media/corona_rating_app.8f2979b7.png"
+  }
+]);
